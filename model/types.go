192d42e97f9191ee5ff56a7e2467846da6ba135e
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2026-02-25 00:00:53 +0530

    feat: The Model added for the types part

--- model/types.go
+++ model/types.go
@@ -1,1 +1,15 @@
 package model
+
+import "time"
+
+type NodeInfo struct {
+	ID       string
+	Address  string
+	LastSeen time.Time
+	Status   string
+}
+
+type Shard struct {
+	ID     int
+	Leader string
+}
